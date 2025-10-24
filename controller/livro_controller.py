b582385621d5641b9b154b0120f16986a0435939
hneri <hilmer.neri@gmail.com>
2025-10-24 20:02:56 -0300

    Adiciona parametro em listar_livro

--- controller/livro_controller.py
+++ controller/livro_controller.py
@@ -1,19 +1,21 @@
 # Material traduzido e adaptado de:
-# Marco Tulio Valente. Engenharia de Software Moderna: Princípios e Práticas para Desenvolvimento de Software com Produtividade,     Editora: Independente, 2020.
+# Marco Tulio Valente. Engenharia de Software Moderna: Princípios e Práticas para Desenvolvimento de Software com Produtividade, Editora: Independente, 2020.
 # UnB-FGA-EPS-MDS
 
 from dao.db_connection import conectar
 from dao.livro_dao import LivroDAO
 
-def listar_livros():
+def listar_livro(autor):
     conn = conectar()
     dao = LivroDAO(conn)
-    livros = dao.listar()
+    livros = dao.pesquisar_por_autor(autor)
     conn.close()
-    html = '<h1>Lista de Livros</h1><ul>'
+    html = '<h4>Dados do Livro Pesquisado</h4><ul>'
     if livros:
         for livro in livros:
-            html += f'<li>{livro.titulo} - {livro.autor}</li>'
+            html += (f'<li>Título: {livro.titulo}</li>'
+                     f'</li>Autor: {livro.autor}</li>'
+                     f'</li>ISBN: {livro.isbn}</li>')
         html += '</ul>'
         return html
     return "Livro não encontrado", 404
