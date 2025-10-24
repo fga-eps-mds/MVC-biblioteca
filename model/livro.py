8b77b77b817986e2a33ddac77de4443290626259
hneri <hilmer.neri@gmail.com>
2025-10-24 19:16:09 -0300

    Adiciona livro na nova estrutura de diretorio

---
+++ model/livro.py
@@ -0,0 +1,9 @@
+# Material traduzido e adaptado de:
+# Marco Tulio Valente. Engenharia de Software Moderna: Princípios e Práticas para Desenvolvimento de Software com Produtividade,     Editora: Independente, 2020.
+# UnB-FGA-EPS-MDS
+
+class Livro:
+    def __init__(self, isbn, autor, titulo):
+        self.isbn = isbn
+        self.autor = autor
+        self.titulo = titulo
