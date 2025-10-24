6ad270f7ad861dbd73761db28be3ba107b209382
hneri <hilmer.neri@gmail.com>
2025-10-24 19:12:38 -0300

    Adiciona o padrao de projeto DAO para livro

---
+++ dao/livro_dao.py
@@ -0,0 +1,24 @@
+import psycopg2
+from model.livro import Livro
+
+class LivroDAO:
+    def __init__(self, conexao):
+        self.conexao = conexao
+
+    def pesquisar_por_autor(self, autor):
+        try:
+            cursor = self.conexao.cursor()
+            cursor.execute("SELECT isbn, autor, titulo FROM livros WHERE LOWER(autor) LIKE %s", (f"%{autor.lower()}%",))
+            rows = cursor.fetchall()
+
+            if rows:
+                isbn, autor, titulo = rows
+                #return [Livro(id=row[0], titulo=row[1], autor=row[2]) for row in rows]
+                return Livro(isbn, autor, titulo)
+            return None
+        except psycopg2.Error as e:
+            print(f"Erro no banco de dados: {e}")
+            return None
+        finally:
+            if self.conexao:
+                cursor.close()
